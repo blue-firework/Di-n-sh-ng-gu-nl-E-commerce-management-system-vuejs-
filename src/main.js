0e08a3b59c2d2d77cf1531cf20674a1dda5e7b89
blue-firework <2052128963@qq.com>
2021-03-31 16:14:43 +0800

    完成了登录页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+//导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://106.53.73.30:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
