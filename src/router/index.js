b61a9037b18fe09400c31818926c3a50eb197c84
blue-firework <2052128963@qq.com>
2021-04-01 22:24:12 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/Login.vue'
-import Home from '@/views/Home'
+import Home from '@/views/home/Home'
+import Welcome from '@/views/home/child/Welcome'
+import Users from '@/views/home/child/Users'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home', 
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ] 
+  }
 ]
 
 const router = new VueRouter({
